e43620bddc8823faa09ce0f7e86f8e9aaa290703
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-02 21:04:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,15 +12,17 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
+  array
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop()
+  array
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
